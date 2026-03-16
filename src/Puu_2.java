7f48bdb7f96a7e654ff87f952bf87b29689b699d
dornarajK <devraj.kharal05@gmail.com>
2026-03-16 11:09:23 +0200

    Task 2 Done

---
+++ src/Puu_2.java
@@ -0,0 +1,8 @@
+public class Puu_2 {
+    static void main(String[] args) {
+        System.out.println("   *");
+        System.out.println("  ***");
+        System.out.println(" *****");
+        System.out.println("*******");
+    }
+}
