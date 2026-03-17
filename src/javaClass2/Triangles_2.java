4849816d6ec36ac105990c7fc18bb5968bd85c4e
dornarajK <devraj.kharal05@gmail.com>
2026-03-17 12:51:58 +0200

    Task 2; Triangles

--- src/javaClass2/Triangles_2.java
+++ src/javaClass2/Triangles_2.java
@@ -1,7 +1,19 @@
 package javaClass2;
+import java.util.Scanner;
+//Write a program that asks
+// the user for the lengths of the
+// legs of a right triangle and prints the length of the
+// hypotenuse of the triangle.
 
 public class Triangles_2 {
-    static void main(String[] args) {
+    public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+        double leftLeg = 3;
+        System.out.print("Enter a lengths of the legs of a right triangle: ");
+        double rightLeg = scanner.nextInt();
+
+        double hypotenuse = Math.sqrt(leftLeg * leftLeg + rightLeg * rightLeg);
+        System.out.println("The length of the hypotenuse is: " + hypotenuse);
     }
 }
